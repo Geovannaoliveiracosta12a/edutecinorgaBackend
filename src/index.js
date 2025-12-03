b14384aa548d75d8f3173ef9546f97379f330b61
geovanna de oliveira costa 12a <geovannaoliveiracosta10@gmail.com>
2025-12-02 22:49:27 -0300

    Adição e conexão com a tabela mysql inorga

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import express from "express"
 import cors from "cors"
+import mysql from "mysql2"
 import { persons } from "./persons.js"
+
+const {DB_NAME, DB_USER, DB_PASSWORD, DB_HOST} = process.env
 
 const app = express()
 const port = 3333
 
 app.use(cors())
 app.use(express.json())
+
+
 
 app.get("/", (request, response) => {
     response.json(persons)
@@ -15,11 +20,28 @@ app.get("/", (request, response) => {
 app.post("/cadastrar", (request, response) =>{
     const {name, email, age, nickname, password} =request.body.user
 
-    console.log(`${name}, ${email}, ${age}, ${nickname}, ${password}`)
+    const insertCommand = `
+        INSERT INTO inorga(name, email, age, nickname, password)
+        VALUES (?, ?, ?, ?, ?)
+    `
 
-    response.status(201).json({message: "Usuário cadastrado co sucesso!"})
+    database.query(insertCommand, [name, email, age, nickname, password], (error) => {
+        if(error){
+            console.log(error)
+            return
+        }
+        response.status(201).json({ message: "Usuário cadastrado com sucesso!"})
+    })
 })
 
 app.listen(port, () =>{
     console.log(`Servidor rodando na porta: ${port}!`)
 })     
+
+const database = mysql.createPool({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    connectionLimit: 10
+})
